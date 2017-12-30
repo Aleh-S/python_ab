6696d06705f0b7a4147bc828bced85a6016e37e2
Aleh-S <a.shybaila@gmail.com>
2017-12-30 00:15:20 -0800

    final commit of task #3

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contact:
+    def __init__(self, firstname, lastname, title, company, address, hometel, mobile, worktel, fax,
+                              email, email2, email3, address2, phone2, notes):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.hometel = hometel
+        self.mobile = mobile
+        self.worktel = worktel
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
