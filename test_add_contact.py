7f6fed3c3c9642228a5763aa0519d80914468d0a
Aleh-S <a.shybaila@gmail.com>
2017-12-29 22:24:12 -0800

    added comments

--- test_add_contact.py
+++ test_add_contact.py
@@ -18,9 +18,10 @@ class test_add_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_test_add_contact(self):
-        success = True
         wd = self.wd
+        # open homepage
         wd.get("http://localhost:8080/addressbook/")
+        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -28,15 +29,15 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        # open new contact page
         wd.find_element_by_link_text("add new").click()
+        # fill contact form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Aleh")
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
         wd.find_element_by_name("lastname").send_keys("Shybaila")
-        foto = wd.find_element_by_name("photo")
-        foto.send_keys("C:\QA\ABarancev\python_ab\\files\\foto1.jpg")
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
         wd.find_element_by_name("title").send_keys("QA")
@@ -67,20 +68,6 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
         wd.find_element_by_name("email3").send_keys("aleh789@gmail.com")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[5]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[5]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("2004")
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").click()
-        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").is_selected():
-            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2005")
         wd.find_element_by_name("address2").click()
         wd.find_element_by_name("address2").clear()
         wd.find_element_by_name("address2").send_keys("N/A")
@@ -90,11 +77,33 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys("Some notes.")
+        # add photo
+        photo = wd.find_element_by_name("photo")
+        photo.send_keys("C:\QA\ABarancev\python_ab\\files\\foto1.jpg")
+        # add birthday
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[5]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[5]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys("2004")
+        # add anniversary
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys("2005")
+        # submit group creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        # return to home page
         wd.find_element_by_link_text("home").click()
+        # open details page
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[7]/a/img").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
 
     def tearDown(self):
         self.wd.quit()
