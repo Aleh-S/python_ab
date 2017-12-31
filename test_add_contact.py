f33af0faace5ecfe2c257f3b0dd5703e1c010c5c
Aleh-S <a.shybaila@gmail.com>
2017-12-30 19:19:01 -0800

    add_contact - moved wd=self.wd into supporting methods

--- test_add_contact.py
+++ test_add_contact.py
@@ -19,29 +19,33 @@ class test_add_contact(unittest.TestCase):
         self.wd.implicitly_wait(60)
 
     def test_test_add_contact(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.fill_new_contact_data(wd, Contact(firstname="Aleh", lastname="Shybaila", title="QA", company="BBB",
+        self.login(username="admin", password="secret")
+        self.fill_new_contact_data(Contact(firstname="Aleh", lastname="Shybaila", title="QA", company="BBB",
                                    address="123 Main Str, Cupertino, CA 95014", hometel="(408)111-2222",
                                    mobile="(408)111-3333", worktel="(408)111-4444", fax="(408)111-5555",
                                    email="aleh123@gmail.com", email2="aleh345@gmail.com", email3="aleh789@gmail.com",
                                    address2="N/A", phone2="N/A", notes="Some notes."))
-        self.open_details_page(wd)
-        self.logout(wd)
+        self.open_details_page()
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def open_details_page(self, wd):
+    def open_details_page(self):
+        wd = self.wd
         wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[7]/a/img").click()
 
-    def return_to_homepage(self, wd):
+    def return_to_homepage(self):
+        wd = self.wd
         wd.find_element_by_link_text("home").click()
 
-    def submit_new_contact_creation(self, wd):
+    def submit_new_contact_creation(self):
+        wd = self.wd
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def add_new_contact_anniversary(self, wd):
+    def add_new_contact_anniversary(self):
+        wd = self.wd
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[7]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[6]").is_selected():
@@ -50,7 +54,8 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("ayear").clear()
         wd.find_element_by_name("ayear").send_keys("2005")
 
-    def add_new_contact_birthday(self, wd):
+    def add_new_contact_birthday(self):
+        wd = self.wd
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").is_selected():
             wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[6]").click()
         if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[5]").is_selected():
@@ -59,12 +64,14 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("2004")
 
-    def add_new_contact_photo(self, wd, pathtojpg):
+    def add_new_contact_photo(self, pathtojpg):
+        wd = self.wd
         photo = wd.find_element_by_name("photo")
         photo.send_keys(pathtojpg)
 
-    def fill_new_contact_data(self, wd, contact):
-        self.open_new_contact_page(wd)
+    def fill_new_contact_data(self, contact):
+        wd = self.wd
+        self.open_new_contact_page()
         # fill text fields
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -112,17 +119,19 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact.notes)
         # fill not text fields
-        self.add_new_contact_photo(wd, pathtojpg="C:\QA\ABarancev\python_ab\\files\\foto1.jpg")
-        self.add_new_contact_birthday(wd)
-        self.add_new_contact_anniversary(wd)
-        self.submit_new_contact_creation(wd)
-        self.return_to_homepage(wd)
+        self.add_new_contact_photo(pathtojpg="C:\QA\ABarancev\python_ab\\files\\foto1.jpg")
+        self.add_new_contact_birthday()
+        self.add_new_contact_anniversary()
+        self.submit_new_contact_creation()
+        self.return_to_homepage()
 
-    def open_new_contact_page(self, wd):
+    def open_new_contact_page(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd, username, password):
-        self.open_home_page(wd)
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -131,7 +140,8 @@ class test_add_contact(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
 
     def tearDown(self):
