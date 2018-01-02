cb4ee797fb8d8ef878e0f88f1e968ace55a3f941
Aleh-S <a.shybaila@gmail.com>
2018-01-01 20:02:27 -0800

    ContactHelper class created

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.fill_new_contact_data(Contact(firstname="Aleh", lastname="Shybaila", title="QA", company="BBB",
-                               address="123 Main Str, Cupertino, CA 95014", hometel="(408)111-2222",
-                               mobile="(408)111-3333", worktel="(408)111-4444", fax="(408)111-5555",
-                               email="aleh123@gmail.com", email2="aleh345@gmail.com", email3="aleh789@gmail.com",
-                               address2="N/A", phone2="N/A", notes="Some notes."))
-    app.open_details_page()
+    app.contact.fill_data(Contact(firstname="Aleh", lastname="Shybaila", title="QA", company="BBB",
+                                  address="123 Main Str, Cupertino, CA 95014", hometel="(408)111-2222",
+                                  mobile="(408)111-3333", worktel="(408)111-4444", fax="(408)111-5555",
+                                  email="aleh123@gmail.com", email2="aleh345@gmail.com", email3="aleh789@gmail.com",
+                                  address2="N/A", phone2="N/A", notes="Some notes."))
+    app.contact.open_details_page()
     app.session.logout()
 
