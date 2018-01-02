e4752c3dfcb192a54fd52edab6d160fecd5772ac
Aleh-S <a.shybaila@gmail.com>
2018-01-01 22:07:26 -0800

    test_edit_group added

--- fixture/group.py
+++ fixture/group.py
@@ -30,6 +30,28 @@ class GroupHelper:
         #return to group page
         self.return_to_groups_page()
 
+    def edit_first_group(self, group2):
+        wd = self.app.wd
+        self.open_group_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click on Edit group button
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group2.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group2.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group2.footer)
+        # click on update button
+        wd.find_element_by_name("update").click()
+        # return to group page
+        self.return_to_groups_page()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
